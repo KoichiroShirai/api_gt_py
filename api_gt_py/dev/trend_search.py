9a400707d43befebd742a06ee776534248a463ab
KS <test@test.com>
2020-09-17 09:41:38 +0900

    2020-09-17 1st.

---
+++ api_gt_py/dev/trend_search.py
@@ -0,0 +1,21 @@
+# coding: utf_8
+from pytrends.request import TrendReq
+pytrends = TrendReq(hl='ja-JP', tz=360)  # hl : 接続言語 , tz : タイムゾーン
+
+
+
+# 関連KW取得
+kw = [input('input kw: ')]
+pytrends.build_payload(kw, cat=0, timeframe='today 5-y', geo='JP', gprop='')
+
+res = pytrends.interest_over_time()
+print(res)
+
+import pandas as pd
+df = pd.DataFrame(res)
+
+k = ''.join(kw)
+
+import datetime
+now = datetime.datetime.now()
+df.to_csv('dev/'+ k + '_' + now.strftime('%Y%m%d_%H%M%S') + '.csv')
